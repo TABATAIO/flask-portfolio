ae0b70faa49bc3aea0c23de3b05541cf7f519237
IoTabata <io.tabata.0513@gmail.com>
2024-11-20 22:21:25 +0900

    自作ポートフォリオを静的から動的へ

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.jinja')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+    
+@app.route('/MIGAKE')
+def migake():
+    return render_template('MIGAKE.jinja')
+
+@app.route('/precompany')
+def precompany():
+    return render_template('precompany.jinja')
