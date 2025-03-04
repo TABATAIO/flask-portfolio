588e6b1a99aa7730670a6a56cbb9125d66e65314
田端莞和 <io.tabata.0513@gmail.com>
2025-03-04 11:59:59 +0900

    ローディングの回数制限の実装

--- static/js/my_script.js
+++ static/js/my_script.js
@@ -29,3 +29,30 @@ $(window).on('load',function(){
 
 bar.set(0);
 bar.animate(-1.0);  
+
+
+
+// スプラッシュの表示回数制限（一日３回まで）
+
+document.addEventListener("DOMContentLoaded",() =>{
+  const splash = document.getElementById("splash_container");
+  const maxViews = 3;// 1日3回まで
+  const today = new Date().toString();// 今日の日付を文字列にする
+
+
+  const lastShoeDate = localStorage.getItem("splashLastShowDate");
+  let viewCount = parseInt(localStorage.getItem("splashViewCount")||0);
+
+//次の日になったら更新
+  if (lastShoeDate !== today){
+    viewCount = 0;
+    localStorage.setItem("splashLastShowDate",today);
+  }
+//３回未満なら表示
+  if (viewCount <= maxViews){
+      splash.style.display = "block";
+      localStorage.setItem("splashViewCount", viewCount + 1);
+    } else {
+      splash.style.display = "none";
+  }
+})
